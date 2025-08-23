f2b693f741cc19e0a576421de1884f33415ccd1f
micezer <obiezeh99@gmail.com>
2025-08-23 16:03:22 +0200

    Base de datos para Render 2

--- olympia/asgi.py
+++ olympia/asgi.py
@@ -14,3 +14,21 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olympia.settings')
 
 application = get_asgi_application()
+
+import os
+from django.core.management import call_command
+from django.db import connections
+from django.db.utils import OperationalError
+
+def load_fixtures_if_needed():
+    try:
+        cursor = connections['default'].cursor()
+        cursor.execute("SELECT COUNT(*) FROM auth_user;")
+        count = cursor.fetchone()[0]
+        if count == 0:  # si no hay usuarios, carga la fixture
+            call_command('load_initial_data')
+    except OperationalError:
+        # La DB no está lista, Django la migrará primero
+        pass
+
+load_fixtures_if_needed()
