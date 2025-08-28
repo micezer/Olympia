713b7b0829abecbe4224827e3d5f31af628dfbac
micezer <obiezeh99@gmail.com>
2025-08-28 12:12:07 +0200

    fix: Update Copa de la Reina modal

--- base/admin.py
+++ base/admin.py
@@ -51,3 +51,14 @@ class PlayerAdmin(admin.ModelAdmin):
     list_display = ("name", "team", "position", "number", "is_staff")
     list_filter = ("team", "position", "is_staff")
     search_fields = ("name", "full_name", "role")
+
+
+# admin.py
+from django.contrib import admin
+from .models import PlayerRegistration
+
+@admin.register(PlayerRegistration)
+class PlayerRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dni', 'birthday', 'created_at')
+    search_fields = ('name', 'dni')
+    list_filter = ('created_at',)
